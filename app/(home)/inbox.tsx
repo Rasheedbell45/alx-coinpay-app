387e27fc5be535a3372cdbb4679ffa9ce6d19523
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-04 05:32:54 +0100

    Create inbox.tsx

---
+++ app/(home)/inbox.tsx
@@ -0,0 +1,9 @@
+// app/(home)/inbox.tsx
+import { View, Text } from "react-native";
+export default function Inbox() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Inbox Screen</Text>
+    </View>
+  );
+}
