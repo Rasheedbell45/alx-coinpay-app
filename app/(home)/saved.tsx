041d160e2627b70f5c00ce5b8efaa09d54c66337
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-09-04 05:31:35 +0100

    Create saved.tsx

---
+++ app/(home)/saved.tsx
@@ -0,0 +1,10 @@
+// app/(home)/saved.tsx
+import { View, Text } from "react-native";
+
+export default function Saved() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Saved Screen</Text>
+    </View>
+  );
+}
